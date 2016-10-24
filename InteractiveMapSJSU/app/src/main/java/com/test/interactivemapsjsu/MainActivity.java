92166458d7500f756fc3f409d20089779382b081
Vighnesh Ajay Rege <NehaRege@Nehas-MacBook-Pro.local>
2016-10-23 18:57:49 -0700

    created a new android project for SJSU Interactive Map Application and added Options menu XML file

---
+++ InteractiveMapSJSU/app/src/main/java/com/test/interactivemapsjsu/MainActivity.java
@@ -0,0 +1,57 @@
+package com.test.interactivemapsjsu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.menu_search:
+
+                Toast.makeText(MainActivity.this, "Searching !!!", Toast.LENGTH_SHORT).show();
+
+                return true;
+
+            default:
+
+
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+}
