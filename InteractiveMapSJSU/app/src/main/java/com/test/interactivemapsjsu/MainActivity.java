a83c132856e0230a685bdd8699f351a2ccb8d88a
Vighnesh Ajay Rege <NehaRege@Nehas-MacBook-Pro.local>
2016-10-25 18:57:56 -0700

    added google play's location services

--- InteractiveMapSJSU/app/src/main/java/com/test/interactivemapsjsu/MainActivity.java
+++ InteractiveMapSJSU/app/src/main/java/com/test/interactivemapsjsu/MainActivity.java
@@ -1,10 +1,20 @@
 package com.test.interactivemapsjsu;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,7 +22,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+
+    private String TAG = "MainActivity";
+
+    public static int ACCESS_LOCATION_REQUEST_CODE = 323;
+
+    public Location lastLocation;
+    public String latitude, longitude;
+
 
     private Button buttonKing;
     private Button buttonEng;
@@ -21,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonYUH;
     private Button buttonSU;
 
+    private GoogleApiClient googleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +53,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setViews();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
 
         buttonKing.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,8 +75,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
+//        @Override
+//        public void onConnected(Bundle connectionHint) {
+//            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+//                    mGoogleApiClient);
+//            if (mLastLocation != null) {
+//                mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+//                mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+//            }
+//        }
 
 
 
@@ -81,6 +119,87 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+
+    }
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == ACCESS_LOCATION_REQUEST_CODE) {
+            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+
+                // permission granted !
+                // save the location !
+
+                saveLocation();
+
+            } else {
+                // permission denied !
+            }
+        }
+    }
+
+    private void saveLocation() {
+
+
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},ACCESS_LOCATION_REQUEST_CODE);
+            return;
+
+        }
+
+        lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+        if(lastLocation != null) {
+
+            latitude = String.valueOf(lastLocation.getLatitude());
+            longitude = String.valueOf(lastLocation.getLongitude());
+
+            Log.i(TAG, "saveLocation: lat = "+latitude);
+            Log.i(TAG, "saveLocation: longitude = "+longitude);
+
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(getString(R.string.location_services_lat),latitude);
+            editor.putString(getString(R.string.location_services_long),longitude);
+            editor.commit();
+
+        } else {
+            Log.i(TAG, "saveLocation: lastlocation is null");
+        }
+
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        saveLocation();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 
