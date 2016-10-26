af0ab781ad4ccfdb77230ed17adef60a2ef72a5f
Vighnesh Ajay Rege <NehaRege@Nehas-MacBook-Pro.local>
2016-10-25 17:05:57 -0700

    added buttons for different buildings in the main activity xml

--- InteractiveMapSJSU/app/src/main/java/com/test/interactivemapsjsu/MainActivity.java
+++ InteractiveMapSJSU/app/src/main/java/com/test/interactivemapsjsu/MainActivity.java
@@ -1,24 +1,44 @@
 package com.test.interactivemapsjsu;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonKing;
+    private Button buttonEng;
+    private Button buttonGarage;
+    private Button buttonBBC;
+    private Button buttonYUH;
+    private Button buttonSU;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+        setViews();
+
+        buttonKing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "KING !!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(toolbar);
+
+
+
 
 
 
@@ -52,6 +72,25 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+
+    }
+
+    public void setViews() {
+
+        buttonBBC = (Button) findViewById(R.id.button_bbc);
+        buttonEng = (Button) findViewById(R.id.button_engg);
+        buttonGarage = (Button) findViewById(R.id.button_garage);
+        buttonSU = (Button) findViewById(R.id.button_su);
+        buttonYUH = (Button) findViewById(R.id.button_yuh);
+        buttonKing = (Button) findViewById(R.id.button_king);
+
+        buttonKing.setVisibility(View.VISIBLE);
+        buttonKing.setBackgroundColor(Color.TRANSPARENT);
+
+
+
+
+
 
     }
 }
